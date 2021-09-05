2c82b0435dac47d1b7634085c06ccf830a5159cd
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-09-05 15:27:10 +0300

    added signin and signup routes into App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,19 @@ import {
   Route
 } from "react-router-dom";
 import Navbar from './components/Navbar';
+import Signin from './pages/Auth/Signin';
+import Signup from './pages/Auth/Signup';
 
 function App() {
   return (
     <Router>
       <div>
         <Navbar />
-
         <div id="content">
           <Switch>
-            <Route path="/" component={Home}>
-            </Route>
+            <Route path="/" exact component={Home} />
+            <Route path="/signin" component={Signin} />
+            <Route path="/signup" component={Signup} />
           </Switch>
         </div>
       </div>
