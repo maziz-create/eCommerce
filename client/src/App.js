fb40a1efb1ae79554a26c4c0a3a6426aa57d4cd7
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-09-05 14:37:47 +0300

    Added react-router module

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div>
+        <Navbar />
 
-    </div>
+        <div id="content">
+          <Switch>
+            <Route path="/" component={Home}>
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
 }
 
 export default App;
