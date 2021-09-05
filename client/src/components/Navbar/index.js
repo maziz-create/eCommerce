5fcb0a2d26d69e4a0ee761c55be16916b9931c16
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-09-05 15:24:06 +0300

    Added navbar component and added right and left section into it. Added logo and menu field into left section. Added signin and signup button into right section

---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import styles from './styles.module.css';
+import { Button } from "@chakra-ui/react";
+
+function Navbar() {
+    return (
+        <nav className={styles.nav}>
+            <div className={styles.left}>
+                <div className="logo">
+                    <Link to="/">eCommerce</Link>
+                </div>
+                <ul className={styles.menu}>
+                    <li>
+                        <Link to="/">Products</Link>
+                    </li>
+                </ul>
+            </div>
+            <div className={styles.right}>
+                <Link to="/signin">
+                    <Button style={{ marginRight: 5 }} colorScheme="pink">Login</Button>
+                </Link>
+                <Link to="/signup">
+                    <Button colorScheme="blue">Register</Button>
+                </Link>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
