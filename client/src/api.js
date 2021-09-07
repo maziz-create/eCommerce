c26f1f835678b10f8ea002ace819acef5166fc4a
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-09-07 16:34:43 +0300

    Added FetchProductDetail function. Api address moved from api.js to .env file.

--- client/src/api.js
+++ client/src/api.js
@@ -1,8 +1,14 @@
 // All api calls will be made here
 import axios from 'axios'
 
-export const fetchProductList = async() => {
-    const { data } = await axios.get('http://localhost:4000/product');
+export const fetchProductList = async () => {
+    const { data } = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/product`);
+
+    return data;
+}
+
+export const fetchProductDetail = async (productId) => {
+    const { data } = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/product/${productId}`);
 
     return data;
 }
