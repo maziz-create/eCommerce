98f65670190ca21f55dd74dbd66cf419d5175685
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-09-08 19:06:54 +0300

    Added axios.interceptors.request.use function because every request must have access-token. Added fetchMe function because login managing(this request includes some user details). Added fetchLogout function for Logout Button in Profile Page

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,27 @@
 // All api calls will be made here
 import axios from 'axios'
 
+// Add a request interceptor
+// Alttaki methodu yazma amacımız token ile auth/me'ye istekler yapmak. tokeni isteğin headers'ına eklemek. 
+axios.interceptors.request.use(
+    function (config) {
+        const { origin } = new URL(config.url);
+
+        //hangi endpointlere istek yapılırken bu eklenmeli ? içinde react_app_base_endpoint varsa ekle..
+        const allowedOrigins = [process.env.REACT_APP_BASE_ENDPOINT]
+        const token = localStorage.getItem('access-token'); //tokeni al.
+
+        if (allowedOrigins.includes(origin)) {
+            config.headers.authorization = token;
+        }
+
+
+        return config;
+    }, function (error) {
+        // Do something with request error
+        return Promise.reject(error);
+    });
+
 //pageParam => sayfa parametresi. Ürünlerin sayfa sayfa gösterilmesini sağlıyoruz.
 export const fetchProductList = async ({ pageParam = 1 }) => {
     const { data } = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/product?page=${pageParam}`);
@@ -22,3 +43,21 @@ export const fetchRegister = async (input) => {
 
     return data;
 }
+
+export const fetchMe = async () => {
+    const { data } = await axios.get(
+        `${process.env.REACT_APP_BASE_ENDPOINT}/auth/me`);
+
+    return data;
+}
+
+export const fetchLogout = async () => {
+    const { data } = await axios.post(
+        `${process.env.REACT_APP_BASE_ENDPOINT}/auth/logout`,
+        {
+            refresh_token: localStorage.getItem('refresh-token')
+        });
+
+    return data;
+}
+
