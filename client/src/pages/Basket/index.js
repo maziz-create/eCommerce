ea8d9d1d2d817a875e23d83619675660154edb3c
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-09-09 15:54:39 +0300

    When deleted a product from basket, It will also delete from localStorage.

--- client/src/pages/Basket/index.js
+++ client/src/pages/Basket/index.js
@@ -1,5 +1,7 @@
 import { useBasket } from '../../contexts/BasketContext'
 
+import { useEffect } from 'react'
+
 import { Link } from 'react-router-dom'
 
 import { Alert, Text, Image, Button, Box } from '@chakra-ui/react'
@@ -8,48 +10,57 @@ function Basket() {
     const { items, removeFromBasket } = useBasket();
     console.log("items =>", items);
 
-    //ürünlerin toplam fiyatını hesaplıyoruz. En sondaki sıfır başlangıç değer.
-    const total = items.reduce((acc, obj) => acc + obj.price, 0); //acc => o ana kadarki toplam.
+    useEffect(() => {
+        localStorage.setItem('basket', JSON.stringify(items));
+}, [items])
 
-    return (
-        <Box p="5">
-            {
-                items.length < 1 && <Alert fontSize="18" py="5" status="warning">
-                    <Text fontWeight="bold" style={{ marginLeft: "auto", marginRight: "auto" }}>Sepette ürün yok!</Text>
-                </Alert>
-            }
+//ürünlerin toplam fiyatını hesaplıyoruz. En sondaki sıfır başlangıç değer.
+const total = items.reduce((acc, obj) => acc + obj.price, 0); //acc => o ana kadarki toplam.
 
-            {
-                items.length > 0 && (
-                    <>
-                        <ul>
-                            {
-                                items.map((item) => {
-                                    return <li style={{ marginBottom: 35 }} key={item._id}>
-                                        <Link to={`/product/${item._id}`}>
-                                            {item.title} - {item.price} TL
-                                            <Image style={{ marginTop: 7, display: 'inline-block' }} htmlWidth={200} src={item.photos[0]} alt="Basket item" />
-                                        </Link>
+return (
+    <Box p="5">
+        {
+            items.length < 1 && <Alert fontSize="18" py="5" status="warning">
+                <Text fontWeight="bold" style={{ marginLeft: "auto", marginRight: "auto" }}>Sepette ürün yok!</Text>
+            </Alert>
+        }
 
-                                        <Button mt="2" size="sm" colorScheme="pink" onClick={() => removeFromBasket(item._id)}>
-                                            Remove From Basket
-                                        </Button>
-                                    </li>
-                                })
-                            }
-                        </ul>
+        {
+            items.length > 0 && (
+                <>
+                    <ul>
+                        {
+                            items.map((item) => {
+                                return <li style={{ marginBottom: 35 }} key={item._id}>
+                                    <Link to={`/product/${item._id}`}>
+                                        {item.title} - {item.price} TL
+                                        <Image 
+                                        style={{ marginTop: 7, display: 'inline-block' }} 
+                                        htmlWidth={200} src={item.photos[0]} 
+                                        alt="Basket item" 
+                                        loading="lazy"
+                                        />
+                                    </Link>
 
-                        <Box mt="10">
-                            <Text fontSize="22">
-                                Total: {total} TL
-                            </Text>
-                        </Box>
-                    </>
-                )
-            }
+                                    <Button mt="2" size="sm" colorScheme="pink" onClick={() => removeFromBasket(item._id)}>
+                                        Remove From Basket
+                                    </Button>
+                                </li>
+                            })
+                        }
+                    </ul>
 
-        </Box>
-    )
+                    <Box mt="10">
+                        <Text fontSize="22">
+                            Total: {total} TL
+                        </Text>
+                    </Box>
+                </>
+            )
+        }
+
+    </Box>
+)
 }
 
 export default Basket
