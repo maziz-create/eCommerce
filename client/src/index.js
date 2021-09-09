146d6c9ddb01d41fadeef51bac27b031b9cd3ef9
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-09-09 10:29:10 +0300

    Added BasketProvider in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,7 @@ import reportWebVitals from './reportWebVitals';
 
 //contexts
 import { AuthProvider } from './contexts/AuthContext'
+import { BasketProvider } from './contexts/BasketContext'
 
 import { ChakraProvider } from "@chakra-ui/react"
 import { QueryClient, QueryClientProvider } from 'react-query'
@@ -27,7 +28,9 @@ ReactDOM.render(
     <QueryClientProvider client={queryClient}>
       <ChakraProvider>
         <AuthProvider>
-          <App />
+          <BasketProvider>
+            <App />
+          </BasketProvider>
         </AuthProvider>
       </ChakraProvider>
 
