c720d2b48f7d35bc1d7a70f1952644aa43efbc7f
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-09-09 10:34:52 +0300

    Added button which will add item to the basket. If this product already added in the basket, it will be removed from basket

--- client/src/pages/ProductDetail/index.js
+++ client/src/pages/ProductDetail/index.js
@@ -4,21 +4,27 @@ import { fetchProductDetail } from '../../api'
 import { Box, Text, Button } from '@chakra-ui/react'
 import moment from 'moment'
 import ImageGallery from 'react-image-gallery';
+import { useBasket } from '../../contexts/BasketContext'
 
 function ProductDetail() {
     const { product_id } = useParams();
+    const { addToBasket, items } = useBasket(); //items'i alma sebebimiz butona tıkladığımızda tekrar tekrar sepete eklemesi.
     const { isLoading, error, data } = useQuery(['product', product_id], () => fetchProductDetail(product_id)
     )
     if (isLoading) return <div>Loading...</div>
 
     if (error) return <div>An error has occured {error.message}</div>
 
-
+    const findBasketItem = items.find((item) => item._id === product_id); //ilgili ürün zaten sepetteyse...
     const images = data.photos.map((url) => ({ original: url }))
 
     return (
         <div style={{ textAlign: "center" }}>
-            <Button style={{ marginBottom: 15 }} colorScheme="pink">Add to basket</Button>
+            <Button style={{ marginBottom: 15 }} colorScheme={findBasketItem ? 'pink' : 'green'} onClick={() => addToBasket(data, findBasketItem)}>
+                {
+                    findBasketItem ? 'Remove from basket' : 'Add to basket'
+                }
+            </Button>
             <Text style={{ marginBottom: 15 }} as="h2" fontSize="2xl">
                 {data.title}
             </Text>
