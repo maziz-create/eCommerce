87e626d58842a326d30ed2fde089a0520123f955
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-09-09 22:32:20 +0300

    Imported some chakraUI items, useRef and useDisclosure for modal(for post order). Added address state for address textarea. Added handleSubmitForm function for form-action of Modal Form. Passed address and product ids which in basket to postOrder api call function. Added emptyBasket function integration in here because after form submit, modal must close and products in the basket should be reset.

--- client/src/pages/Basket/index.js
+++ client/src/pages/Basket/index.js
@@ -1,66 +1,146 @@
 import { useBasket } from '../../contexts/BasketContext'
 
-import { useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 import { Link } from 'react-router-dom'
 
-import { Alert, Text, Image, Button, Box } from '@chakra-ui/react'
+import {
+    Alert,
+    Text,
+    Image,
+    Button,
+    Box,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    useDisclosure,
+    FormLabel,
+    Textarea,
+    FormControl
+} from '@chakra-ui/react'
+import { postOrder } from '../../api';
 
 function Basket() {
-    const { items, removeFromBasket } = useBasket();
+    //for modal
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const initialRef = useRef(); //açıldığında inputa focuslanması için.
+
+    //sipariş verebilmek için adres yazılacak, yazılan adresi state'e atayacağız.
+    const [address, setAddress] = useState("");
+    //modal için save butonuna bastığımızdaki action kısmını hazırlıyoruz.
+    const handleSubmitForm = async () => {
+        // console.log("submit button");
+
+        //sepete eklenen ürünlerin Id'lerini ayarlamalıyız.
+        const itemIds = items.map((item) => item._id);
+
+        //backend input bekliyor. onu hazırlayacağız.
+        const input = {
+            address,
+            items: JSON.stringify(itemIds),
+        }
+
+        const responseOrder = await postOrder(input);
+        emptyBasket();
+        onClose();
+
+
+        console.log(itemIds);
+        console.log(responseOrder);
+    }
+
+    //for basket(items state came from BasketContext)
+    const { items, removeFromBasket, emptyBasket } = useBasket();
     console.log("items =>", items);
 
     useEffect(() => {
         localStorage.setItem('basket', JSON.stringify(items));
-}, [items])
+    }, [items])
 
-//ürünlerin toplam fiyatını hesaplıyoruz. En sondaki sıfır başlangıç değer.
-const total = items.reduce((acc, obj) => acc + obj.price, 0); //acc => o ana kadarki toplam.
+    //ürünlerin toplam fiyatını hesaplıyoruz. En sondaki sıfır başlangıç değer.
+    const total = items.reduce((acc, obj) => acc + obj.price, 0); //acc => o ana kadarki toplam.
 
-return (
-    <Box p="5">
-        {
-            items.length < 1 && <Alert fontSize="18" py="5" status="warning">
-                <Text fontWeight="bold" style={{ marginLeft: "auto", marginRight: "auto" }}>Sepette ürün yok!</Text>
-            </Alert>
-        }
+    return (
+        <Box p="5">
+            {
+                items.length < 1 && <Alert fontSize="18" py="5" status="warning">
+                    <Text fontWeight="bold" style={{ marginLeft: "auto", marginRight: "auto" }}>Sepette ürün yok!</Text>
+                </Alert>
+            }
 
-        {
-            items.length > 0 && (
-                <>
-                    <ul>
-                        {
-                            items.map((item) => {
-                                return <li style={{ marginBottom: 35 }} key={item._id}>
-                                    <Link to={`/product/${item._id}`}>
-                                        {item.title} - {item.price} TL
-                                        <Image 
-                                        style={{ marginTop: 7, display: 'inline-block' }} 
-                                        htmlWidth={200} src={item.photos[0]} 
-                                        alt="Basket item" 
-                                        loading="lazy"
+            {
+                items.length > 0 && (
+                    <>
+                        <ul>
+                            {
+                                items.map((item) => {
+                                    return <li style={{ marginBottom: 35 }} key={item._id}>
+                                        <Link to={`/product/${item._id}`}>
+                                            {item.title} - {item.price} TL
+                                            <Image
+                                                style={{ marginTop: 7, display: 'inline-block' }}
+                                                htmlWidth={200} src={item.photos[0]}
+                                                alt="Basket item"
+                                                loading="lazy"
+                                            />
+                                        </Link>
+
+                                        <Button mt="2" size="sm" colorScheme="pink" onClick={() => removeFromBasket(item._id)}>
+                                            Remove From Basket
+                                        </Button>
+                                    </li>
+                                })
+                            }
+                        </ul>
+
+                        <Box mt="10">
+                            <Text fontSize="22">
+                                Total: {total} TL
+                            </Text>
+                        </Box>
+
+                        <Button mt="2" size="sm" colorScheme="pink" onClick={onOpen}>Order</Button>
+
+                        <Modal
+                            initialFocusRef={initialRef}
+                            isOpen={isOpen}
+                            onClose={onClose}
+                        >
+                            <ModalOverlay />
+                            <ModalContent>
+                                <ModalHeader>Create your account</ModalHeader>
+                                <ModalCloseButton />
+                                <ModalBody pb={6}>
+                                    <FormControl>
+                                        <FormLabel>Address</FormLabel>
+                                        <Textarea
+                                            ref={initialRef}
+                                            placeholder="Address"
+                                            value={address}
+                                            onChange={(e) => setAddress(e.target.value)}
                                         />
-                                    </Link>
+                                    </FormControl>
+                                </ModalBody>
 
-                                    <Button mt="2" size="sm" colorScheme="pink" onClick={() => removeFromBasket(item._id)}>
-                                        Remove From Basket
+                                <ModalFooter>
+                                    <Button colorScheme="blue" mr={3} onClick={handleSubmitForm}>
+                                        Save
                                     </Button>
-                                </li>
-                            })
-                        }
-                    </ul>
+                                    <Button onClick={onClose}>Cancel</Button>
+                                </ModalFooter>
+                            </ModalContent>
+                        </Modal>
 
-                    <Box mt="10">
-                        <Text fontSize="22">
-                            Total: {total} TL
-                        </Text>
-                    </Box>
-                </>
-            )
-        }
+                    </>
+                )
+            }
 
-    </Box>
-)
+        </Box>
+    )
 }
 
 export default Basket
