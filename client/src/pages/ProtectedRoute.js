c788b81d7316103395b9de36c1e873ab46d1900b
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-09-10 09:17:26 +0300

    If user is not admin, it will redirected to home page.

--- client/src/pages/ProtectedRoute.js
+++ client/src/pages/ProtectedRoute.js
@@ -1,18 +1,23 @@
 import { Route, Redirect } from 'react-router-dom'
 import { useAuth } from '../contexts/AuthContext'
 
-//component => gösterilecek ekrange
+//component => gösterilecek ekran. Component olarak almazsak aşağıda çalışmıyor.
 //rest => girilmiş property varsa hepsini rest altında topluyoruz.
-function ProtectedRoute({ component: Component, ...rest }) {
-    const { loggedIn } = useAuth();
+function ProtectedRoute({ component: Component, admin, ...rest }) { //admin propunu aldık.
+    const { loggedIn, user } = useAuth();
     return (
         <Route {...rest} render={(props) => {
+
+            if (admin && user.role !== 'admin') {
+                return <Redirect to={{ pathname: "/" }} />
+            }
+
             if (loggedIn) {
                 return <Component {...props} />
             }
-            return <Redirect to="/" />
+            return <Redirect to={{ pathname: "/" }} /> //direkt to="/" olarak da yazılabiliyor.
         }} />
     )
 }
 
-export default ProtectedRoute
+export default ProtectedRoute   
