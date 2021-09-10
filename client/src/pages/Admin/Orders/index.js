f8fed7c0dbb57f8ded9ee9e326868e16418be7c3
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-09-10 10:12:05 +0300

    Orders component's content is filled. Admin can see the email address of the person who ordered, the order address and how many product user bought.

--- client/src/pages/Admin/Orders/index.js
+++ client/src/pages/Admin/Orders/index.js
@@ -1,9 +1,57 @@
 import React from 'react'
+import { useQuery } from 'react-query'
+import { fetchOrders } from '../../../api'
+
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    Text
+} from '@chakra-ui/react'
+
 
 function Orders() {
+    const { isLoading, isError, data, error } = useQuery('admin:orders', fetchOrders);
+
+    if (isLoading) {
+        return <div>Loading...</div>
+    }
+
+    if (isError) {
+        return <div>Error: {error}</div>
+    }
+
+    console.log(data);
+
     return (
         <div>
-            Orders Component
+            <Text fontSize="2xl" p={5}>Orders</Text>
+
+            <Table variant="simple">
+                <TableCaption>Genel açıklama</TableCaption>
+                <Thead>
+                    <Tr>
+                        <Th>User</Th>
+                        <Th>Address</Th>
+                        <Th isNumeric>Items</Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    {
+                        data.map((item) => (
+                            <Tr key={item._id}>
+                                <Td>{item.user.email}</Td>
+                                <Td>{item.adress}</Td>
+                                <Td isNumeric>{item.items.length}</Td>
+                            </Tr>
+                        ))
+                    }
+                </Tbody>
+            </Table>
         </div>
     )
 }
